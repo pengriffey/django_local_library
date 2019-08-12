d8a45c0483a6cf5461d517d83a31d6cfb6d67a4e
unknown <qgdeon@live.com>
2019-08-12 11:59:06 +0000

    First version of the app moved into github

---
+++ catalog/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Language, Book, BookInstance
+
+# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'id', 'status', 'due_back', 'borrower',)
+    list_filter = ('book', 'status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
